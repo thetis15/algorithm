6af1944f3fa6543f548d13153836418fb095f039
thetis0105 <thetis0105@gmail.com>
2025-07-27 12:37:32 +0900

    first commit

---
+++ src/test/java/JustTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JustTest {
+
+    @Test
+    void helloTest() {
+        var hello = "world";
+        assertTrue(true);
+        assertEquals(hello, "world");
+    }
+}
